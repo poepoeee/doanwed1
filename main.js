3d0416c5d8ae74015661a0e5a4c76a0fa2a32885
Hoang Hiep <nguyenhoanghiep478@gmail.com>
2022-10-01 10:00:00 +0700

    header wed

---
+++ main.js
@@ -0,0 +1,35 @@
+var index = 1;
+var i = 1;
+function changeimg () {
+    var imgs = ["slideshow_1.webp","slideshow_2.webp","slideshow_3.webp"];
+    document.getElementById("img-slider").src = imgs[index];
+    index++;
+    if (index == 3)
+    {
+        index = 0;
+    }
+}
+setInterval(changeimg,2000);
+
+function showDiv (k)
+{
+    var imgs = ["slideshow_1.webp","slideshow_2.webp","slideshow_3.webp"];
+    document.getElementById("img-slider").src = imgs[k-1];
+}
+
+function showDivs(k) {
+    var i;
+    var imgs = ["slideshow_1.webp","slideshow_2.webp","slideshow_3.webp"];
+    if (k > 2) {index = 1}
+    if (k < 1) {index = 2}
+    for (i = 0; i < 3; i++) {
+      document.getElementById("img-slider").src = imgs[i];
+    }
+    document.getElementById("img-slider").src = imgs[index-1];  
+  }
+
+function plusDivs (k)
+{
+    showDivs(index += k);
+}
+
