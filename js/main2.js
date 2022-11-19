07bdac112c19e840a1b22ec9b32324357067a57a
unknown <chuongle.nt20@gmail.com>
2022-11-19 13:20:58 +0700

    chuong lam

---
+++ js/main2.js
@@ -0,0 +1,54 @@
+import { products } from "./json.js";
+
+// Điều khiển nút bấm trên Section
+const sectionItems = document.querySelectorAll(".list-product li");
+
+sectionItems.forEach((sectionItem, index) => {
+  sectionItem.onclick = () => {
+    document
+      .querySelector(".list-product li.active")
+      .classList.remove("active");
+    sectionItem.classList.add("active");
+    renderProduct(products[index]);
+  };
+});
+
+// Render sản phẩm ra màn hình
+function renderProduct(products) {
+  const htmls = products
+    .map((item) => {
+      return `<li class="card" style="">
+        <img src='${item.image}' style= {}">
+        <div class="card-body">
+        <div class="card-reviews">
+        <i class="ti-star"></i>
+        <i class="ti-star"></i>
+        <i class="ti-star"></i>
+        <i class="ti-star"></i>
+        <i class="ti-star"></i>
+        <p class="status-review">
+            review
+        </p>
+        </div>
+    <div class="card-info">
+        <span class="info-name">
+            ${item.name}
+            ${item.weight}
+        </span>
+        <span class="info-price">
+            ${item.price}
+        </span>
+    </div>
+    </div>`;
+    })
+    .join("");
+  document.querySelector(".right").innerHTML = htmls;
+}
+
+const cards = document.querySelectorAll(".card img");
+cards.forEach(function (item) {
+  item.addEventListener("mouseover", () => {
+    item.style.width = "100%";
+    item.style.height = "100%";
+  });
+});
