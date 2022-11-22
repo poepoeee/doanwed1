e74d6b5f44953bd514e41c298af42ef49d6fca3f
unknown <nguyenuoanghiep4790>
2022-11-22 16:12:10 +0700

    hiep lam

---
+++ js/searchFunction.js
@@ -0,0 +1,345 @@
+var products = [
+    
+    {
+        image: '../access/img/img_1.webp',
+        name: 'Peruvian Organic Raw Cacao Butter (250g)',
+        price: '£7.49', 
+        category: 'BRANDS'
+    },
+    {
+       image: '../access/img/img_1.webp',
+       name: 'Peruvian Organic Raw Cacao Butter (250g)',
+       price: '£7.49' ,
+       category: 'BRANDS'
+    },
+    {
+       image: '../access/img/img_1.webp',
+       name: 'Peruvian Organic Raw Cacao Butter (250g)',
+       price: '£7.49',
+       category: 'Consious Chocolate'
+    },
+    {
+       image: '../access/img/img_1.webp',
+        name: 'Peruvian Organic Raw Cacao Butter (250g)',
+        price: '£7.49' ,
+        category:'Element for life'
+   },
+   {
+       image: '../access/img/img_1.webp',
+        name: 'Peruvian Organic Raw Cacao Butter (250g)',
+        price: '£7.49' ,
+        category: 'Enjoy'
+   },
+   {
+       image: '../access/img/img_1.webp',
+        name: 'Peruvian Organic Raw Cacao Butter (250g)',
+        price: '£7.49' ,
+        category:'Forever Cacao'
+   },
+   {
+       image: '../access/img/img_1.webp',
+        name: 'Peruvian Organic Raw Cacao Butter (250g)',
+        price: '£7.49' ,
+        category:'Ombar'
+   },
+   {
+       image: '../access/img/list-img-1.webp',
+       name: 'Conscious Citrus Zest',
+       weight: '60g',
+       price: '£2.99',
+       category:'Ombar'
+   },
+   {
+       image: '../access/img/list-img-2.webp',
+       name: 'Conscious Dark Side 85%',
+       weight: '60g',
+       price: 'Sold out',
+       category:'Forever Cacao'
+   },
+   {
+       image: '../access/img/list-img-3.webp',
+       name: 'Conscious Four Nuts',
+       weight: '60g',
+       price: '£2.99',
+       category:'Element for life'
+   },
+   {
+       image: '../access/img/list-img-4.webp',
+       name: 'Conscious Goji and Coconut',
+       weight: '60g',
+       price: '£2.99',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-5.webp',
+       name: 'Conscious Love Potion No.9',
+       weight: '60g',
+       price: '£2.99',
+       category: 'Consious Chocolate'
+   },
+   {
+       image: '../access/img/list-img-6.webp',
+       name: 'Conscious Maca & Sprirulina',
+       weight: '60g',
+       price: '£2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-7.webp',
+       name: 'Conscious Orange & Tanregine',
+       weight: '60g',
+       price: '£2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-8.webp',
+       name: 'Coracao 81% Cacao Bar',
+       weight: '(2oz/57g)',
+       price: 'Sold out',
+       category: 'Consious Chocolate'
+
+   },
+   {
+       image: '../access/img/list-img-9.avif',
+       name: 'Coracao After Dark Bar',
+       weight: '(2oz/57g)',
+       price: 'Sold out',
+       category: 'Consious Chocolate'
+   },
+   {
+       image: '../access/img/list-img-10.webp',
+       name: 'Coracao Berkeley Bar',
+       weight: '(2.5oz/70g)',
+       price: 'Sold out',
+       category:'Element for life'
+   },
+   {
+       image: '../access/img/list-img-11.webp',
+       name: 'Coracao Caramel Bar',
+       weight: '(2.5oz/70g)',
+       price: 'Sold out',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-12.webp',
+       name: 'Coracao Hazelnut Cream Bar',
+       weight: '(2oz/56g)',
+       price: 'Sold out',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-13.webp',
+       name: 'Yummy Scrummy Raw Chocolate Brownie',
+       price: 'from £2.99',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-14.webp',
+       name: 'Enjoy! Coconut Milk',
+       weight: '(70g)',
+       price: 'from £2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-13.webp',
+       name: 'Yummy Scrummy Raw Chocolate Brownie',
+       weight: ' ',
+       price: 'from £2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-14.webp',
+       name: 'Enjoy! Daringly Darkest 85%',
+       weight: '(70g)',
+       price: 'from £2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-15.webp',
+       name: 'Enjoy! Delightfully Dark 70% (70g)',
+       weight: ' ',
+       price: 'from £2.99',
+       category: 'Consious Chocolate'
+   },
+   {
+       image: '../access/img/list-img-16.webp',
+       name: 'Enjoy! Delightfully Dark 70%',
+       weight: 'Button (96g)',
+       price: 'from £2.99',
+       category: 'Consious Chocolate'
+   },
+   {
+       image: '../access/img/list-img-17.webp',
+       name: 'Enjoy! Magical Mint',
+       weight: 'Button (96g)',
+       price: 'from £2.99',
+       category: 'Consious Chocolate'
+   },
+   {
+       image: '../access/img/list-img-18.webp',
+       name: 'Enjoy! Mint',
+       weight: '(70g)',
+       price: 'from £2.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-19.webp',
+       name: 'Forever Cacao: Bean to Bar 100%)',
+       weight: ' ',
+       price: 'from £3.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-19.webp',
+       name: 'Forever Cacao: Bean to Bar 55%',
+       weight: ' ',
+       price: 'from £3.99',
+       category: 'BRANDS'
+   },
+   {
+       image: '../access/img/list-img-19.webp',
+       name: 'Forever Cacao: Bean to Bar 65%',
+       weight: ' ',
+       price: 'from £3.99',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-19.webp',
+       name: 'Forever Cacao: Bean to Bar 75%',
+       weight: ' ',
+       price: 'from £3.99',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-20.webp',
+       name: 'Ombar 100% Organic Chocolate ',
+       weight: '(35g)',
+       price: 'from £3.99',
+       category: 'Enjoy'
+   },
+   {
+       image: '../access/img/list-img-21.webp',
+       name: 'Ombar 72% Organic Chocolate ',
+       weight: '(35g)',
+       price: 'Sold out',
+       category:'Element for life'
+   },
+   {
+       image: '../access/img/list-img-21.webp',
+       name: 'Ombar 100% Organic Chocolate ',
+       weight: '(70g)',
+       price: 'Sold out',
+       category:'Element for life'
+   },
+   {
+       image: '../access/img/list-img-22.webp',
+       name: 'Ombar Blueberry & Acai Organic Chocolate',
+       weight: '(35g)',
+       price: 'from £1.99',
+       category:'Element for life'
+   }
+]
+if(localStorage.getItem("products")==null){
+
+   products=localStorage.setItem("products",JSON.stringify(products));
+}
+window.onload=addCategory();
+window.onload=addKeyBoardSearch();
+window.onload=showListCategory();
+window.onload=addHideListSearch();
+function addKeyBoardSearch(){
+    let searchKeyboard=document.getElementById("searchInput");
+    searchKeyboard.addEventListener("keypress",function(event){
+    if(event.key==="Enter"){
+        event.preventDefault();
+        let category=document.getElementById("categoryList").value;
+        let minPrice=document.getElementById("minRange").value;
+        let maxPrice=document.getElementById("maxRange").value;
+        localStorage.setItem("findProduct",JSON.stringify(controllerFindProduct(searchKeyboard.value,category,minPrice,maxPrice)));
+        window.location="search.html"
+    }
+    })
+};
+function addHideListSearch(){
+    let listRequired=document.getElementById("searchInput");
+    listRequired.addEventListener('focusin',(event)=>{
+        let listRequired=document.getElementById("requiredContainer");
+        listRequired.setAttribute('class','show-block');
+    });
+    // listRequired.addEventListener('focusout',(event)=>{
+    //     let listRequired=document.getElementById("requiredContainer");
+    //     listRequired.setAttribute('class','hidden');
+    // });
+}
+function controllerFindProduct(productName,category,minPrice,maxPrice){
+    return findProductByName(productName,findProductByRange(minPrice,maxPrice,findProductBycategory(category)));
+}
+//arrow funtion
+
+function findProductBycategory(category){
+    if(category!='All'){
+        let findProduct=[];
+        for(let i =0;i<products.length;i++){
+            if((products[i].category)===category){
+                findProduct.push(products[i]);
+            }
+        }
+        return findProduct;
+    }else{
+        return  products;
+    }
+}
+function findProductByRange(minPrice,maxPrice,beforeList){
+    let findProduct=[];
+    if(maxPrice==''){
+        maxPrice=1.7976931348623157e+308;
+    }
+    if(minPrice==''){
+        minPrice=0;
+    }
+    for(let i =0;i<beforeList.length;i++){
+        if(!(beforeList[i].price).includes('Sold out') && (parseFloat(((beforeList[i].price).split("£")[1])))>=minPrice&&(parseFloat(((beforeList[i].price).split("£")[1])))<=maxPrice){       
+             findProduct.push(beforeList[i]);
+        }
+     }
+    return findProduct;
+}
+function findProductByName(productName,beforeList){
+    let findProduct=[];                                             
+    if(productName!=''){
+        for(let i =0;i<beforeList.length;i++){
+            if((beforeList[i].name).includes(productName)){
+                findProduct.push(beforeList[i]);
+            }
+        }
+        return findProduct;
+    }else{
+        return beforeList;
+    }
+}
+function addCategory(){
+    let category=[
+        "BRANDS","Consious Chocolate","Element for life","Enjoy","Forever Cacao","Ombar"
+    ]
+    localStorage.setItem("categories",JSON.stringify(category));
+}
+function showListCategory(){
+    let categories= JSON.parse(localStorage.getItem("categories"));
+        let categoryList=document.getElementById("categoryList");
+        let category=[categories.length];
+       for(var i=0;i<categories.length;i++){
+             category[i]=document.createElement("option");
+             category[i].setAttribute("value",categories[i]);
+           category[i].innerHTML=categories[i];
+           categoryList.appendChild(category[i]);
+       }
+}
+function showListSearch(){
+    let listRequired=document.getElementById("requiredContainer");
+    listRequired.setAttribute("class","show-flex");
+   
+}
+function hideListSearch(){
+    let listRequired=document.getElementById("requiredContainer");
+    listRequired.setAttribute('class','hidden');
+}
